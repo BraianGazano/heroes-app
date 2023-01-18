89ccebdbbe79576691a5f12334d06d87df4ae47f
Braian Gazano <braian.gazano@accenture.com>
2023-01-18 17:44:31 -0300

    Aplicación funcional terminada

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Navbar } from "../../ui/components";
+import { MarvelPage } from "../pages/MarvelPage";
+import { DCPage } from "../pages/DCPage";
+import { HeroPage } from "../pages/HeroPage";
+import { SearchPage } from "../pages/SearchPage";
+export const HeroesRoutes = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route path="marvel" element={<MarvelPage />} />
+                    <Route path="dc" element={<DCPage />} />
+                    <Route path="search" element={<SearchPage />} />
+                    <Route path="hero/:id" element={<HeroPage />} />
+                    <Route path="/" element={<Navigate to="/marvel" />} />
+                </Routes>
+            </div>
+        </>
+    )
+}
